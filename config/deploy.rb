b1abef8336d2c090125e82363ce350049fb64f19
Shafayet Jamil <shafayetemon88@gmail.com>
2023-04-07 12:02:08 +0600

    nvm_map_bins added

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :nvm_map_bins, %w{node npm yarn webpack rake}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
