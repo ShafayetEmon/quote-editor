6b419438d2e70471622d038ed56957a2cab80448
Shafayet Jamil <shafayetemon88@gmail.com>
2023-04-09 13:46:44 +0600

    deploy.rb file refactored

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,3 +34,15 @@ namespace :puma do
   end
   before :start, :make_dirs
 end
+
+namespace :deploy do
+  task :seed do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
