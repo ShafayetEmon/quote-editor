6eafe781300ede45cc6233e4a54946b8ae8324ce
Shafayet Jamil <shafayetemon88@gmail.com>
2023-04-09 14:03:44 +0600

    deploy.rb file refactored

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,13 +36,14 @@ namespace :puma do
 end
 
 namespace :deploy do
-  task :seed do
-    on primary fetch(:migration_role) do
-      within release_path do
-        with rails_env: fetch(:rails_env)  do
-          execute :rake, 'db:seed'
-        end
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
       end
-    end
   end
+end
 end
